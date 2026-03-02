e53d2bd7018fea22118502e3106659daf255e49e
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-03-02 12:52:02 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "chaap-admin-cache-v1";
+
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/offline.html"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request).catch(() => {
+        if (event.request.destination === "document") {
+          return caches.match("/offline.html");
+        }
+      });
+    })
+  );
+});
